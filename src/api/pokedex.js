f911dbf42823e2e1a58c8e5c86e6d40a6ec3bb61
Pacome <pacomedanilo503@gmail.com>
2022-12-13 15:50:27 +0100

    modif pacome

--- src/api/pokedex.js
+++ src/api/pokedex.js
@@ -1,3 +1,4 @@
+
 export const getAll = async () => {
     const response = await fetch(
         'http://localhost:4444/pokedexUser/list', {
@@ -10,4 +11,24 @@ export const getAll = async () => {
     )
     const pokedex = await response.json()
     return pokedex
+}
+
+export const renamePokedex = async (pokemon , newname) => {
+    const response = await fetch(
+        'http://localhost:4444/pokemon/updatePoke', {
+            method: 'POST', 
+            headers: {
+                'Accept': 'application/json', 
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                'changePokemon': pokemon.name,
+                'name':document.getElementById(newname).value
+
+
+            })
+        }
+    )
+    const pokedexUpdate = await response.json()
+    return pokedexUpdate
 }
--- src/pages/Pokedex.js
+++ src/pages/Pokedex.js
@@ -1,16 +1,17 @@
 
-import { getAll } from "../api/pokedex";
+import { renamePokedex , getAll } from "../api/pokedex";
 import { useState,useEffect } from "react";
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import { Col } from "react-bootstrap";
 import { deletePokedex } from "../api/pokemon";
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
 
   function Pokedex(){
 
     const [ pokedex, setPokedex ] = useState([]);
+    
     //va s'executer seulement au lancement du composant (dep: [])
     useEffect(() => {
     // récupérer la liste des users seulement au chargement du composant ! 
@@ -25,7 +26,7 @@ import Button from 'react-bootstrap/Button';
         <Container className="container">
         <Row >    
         {
-            pokedex.map((pokedex,key) =>{
+            pokedex.map((pokedex,pokemon, key ) =>{
                 return <Card className="pokemon-card" style={{ width: '15rem' }}>
                 <Card.Img variant="top" style={{ width: '100px',height:'95px'}}  src={pokedex.sprites.normal} />
                 <Card.Body>
@@ -37,7 +38,9 @@ import Button from 'react-bootstrap/Button';
                   <br></br>
                   type: {pokedex.type}
                   </Card.Text>
-                  <Button onClick={()=>deletePokedex(pokedex.name)} variant="primary">Supprimer du Pokedex</Button>
+                  <input type='text' id={key} name='name' placeholder='Donnez lui un surnom'/>
+                  <button onClick={()=>renamePokedex(pokemon , key )}>Renommer{pokemon.name} </button>
+                  <button onClick={()=>deletePokedex(pokedex.name)} variant="primary">Supprimer du Pokedex</button>
                 </Card.Body>
               </Card>
             })
