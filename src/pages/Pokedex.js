8580e91c1c3ce8b98471a2d8d778edd52eee6d92
Rémi Salles <remisalles.12@gmail.com>
2022-12-13 17:10:42 +0100

    Supp update

--- src/pages/Pokedex.js
+++ src/pages/Pokedex.js
@@ -1,21 +1,20 @@
-
-import { renamePokedex , getAll } from "../api/pokedex";
 import { useState,useEffect } from "react";
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import { Col } from "react-bootstrap";
 import { deletePokedex } from "../api/pokemon";
-// import Button from 'react-bootstrap/Button';
+import Button from 'react-bootstrap/Button';
+import { getAll } from "../api/pokedex";
 
   function Pokedex(){
-
+    const [count , setCount] = useState(0);
     const [ pokedex, setPokedex ] = useState([]);
     //va s'executer seulement au lancement du composant (dep: [])
     useEffect(() => {
     // récupérer la liste des users seulement au chargement du composant ! 
     const pokedexFetched = getAll();
-   pokedexFetched
+    pokedexFetched
         .then(result => setPokedex(result))
         .catch(error=>console.error("Erreur avec notre API :",error.message));
     },[count]);
@@ -25,7 +24,7 @@ import { deletePokedex } from "../api/pokemon";
         <Container className="container">
         <Row >    
         {
-            pokedex.map((pokedex,pokemon, key ) =>{
+            pokedex.map((pokedex, key ) =>{
                 return <Card className="pokemon-card" style={{ width: '15rem' }}>
                 <Card.Img variant="top" style={{ width: '100px',height:'95px'}}  src={pokedex.sprites.normal} />
                 <Card.Body>
@@ -37,7 +36,7 @@ import { deletePokedex } from "../api/pokemon";
                   <br></br>
                   type: {pokedex.type}
                   </Card.Text>
-                  <Button onClick={()=>deletePokedex(pokedex.name)} variant="primary">Supprimer du Pokedex</Button>
+                  <Button onClick={()=>{deletePokedex(pokedex.name);setCount(count+1)}} variant="primary">Supprimer du Pokedex</Button>
                 </Card.Body>
               </Card>
             })
